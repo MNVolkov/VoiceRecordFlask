4bfde8890d75196e2587a70489b5a8b557704457
MNVolkov <mr-volkov@yandex.ru>
2022-10-31 21:13:46 +0600

    first commit

---
+++ FlaskApp/views.py
@@ -0,0 +1,23 @@
+﻿"""
+Routes and views for the flask application.
+"""
+
+import pandas as pd
+import numpy as np
+
+from datetime import datetime
+from flask import render_template
+from flask import request
+from FlaskApp import app
+
+@app.route('/')
+def input():
+    """Отображает форму ввода данных"""
+    return render_template(
+        'index.html',
+        title='Ввод данных'
+    )
+
+    
+   
+
